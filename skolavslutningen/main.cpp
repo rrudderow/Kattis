93e18c48c47422dcd42e13707c87739b738f1ff2
Renee Rudderow <rrudderow@yahoo.com>
2026-03-17 11:22:51 -0600

    matrix

--- skolavslutningen/main.cpp
+++ skolavslutningen/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 
     int row,col,cl;
+    int count=0;
+    char matrix[row][col];
 
     cin >> row >> col >> cl;
 
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin >> matrix[i][j];
+        }
+    }
 
+
+    cout << count << endl;
 
 
     return 0;
